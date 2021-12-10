f504a4420ec6edc877d94b29e2e8ae9a824362bc
VcatYuki <neusbosk@gmail.com>
2021-12-10 09:53:36 +0100

    Catarina Work

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './main.scss';
+import React from "react";
+import {pokemonList, PokeList } from "./components/pokeList";
+
+export function App() {
+  return (
+    <>
+      <h1>Hey</h1>
+
+      <PokeList 
+      item={pokemonList}
+      />
+      
+    </>
+  )
+}
